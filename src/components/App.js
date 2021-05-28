cf44b3606bc7d55c682c4eb1eea5b2bfd2416a3a
Lukman Sanni <lukmansanni60@gmail.com>
2021-05-28 15:25:20 +0100

    finish header

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+
+import React from "react";
+import "./App.scss";
+
+import Hero from './Hero/Hero';
+
+export default function App() {       
+
+  return (
+    <>
+      <Hero />
+
+      <section className='main'>
+      <h2>9ijakids Games Result</h2>
+
+      </section>
+    </>
+
+
+  );
+}
+
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import styles from './Hero.module.scss';
+
+
+const Hero = () => {
+
+    return (
+        <header className={styles.body}>
+            <h1>9ijakids Game List</h1>
+            <h3>Solution to the Front-End Engineer ReactJS Virtual Internship test.</h3>
+
+            <form className={styles.searchBox}>
+                <input type='search' placeholder='Search for Games by Topic' />
+                <button type="button">
+                    Search
+                </button>
+            </form>
+        </header>
+    )
+};
+
+export default Hero;
